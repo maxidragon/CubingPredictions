9b6223c7be096c5eebb755d4e4c8b15f19a88208
maxidragon <mgwspinanie2006@wp.pl>
2023-06-02 08:03:19 +0200

    Add button to change psychsheet type

--- frontend/src/Pages/Competition/Psychsheet/Psychsheet.tsx
+++ frontend/src/Pages/Competition/Psychsheet/Psychsheet.tsx
@@ -1,26 +1,35 @@
 import classes from "./Psychsheet.module.css";
-import {Box, Link, Table, TableBody, TableCell, TableHead, TableRow, Typography} from "@mui/material";
+import {Box, Button, Link, Table, TableBody, TableCell, TableHead, TableRow, Typography} from "@mui/material";
 import {generateRanking} from "../../../logic/competitions";
 import {useEffect, useState} from "react";
 
 const Psychsheet = (props: any) => {
     const [ranking, setRanking] = useState<any>([]);
+    const [type, setType] = useState<string>('average');
 
     const generateRankingData = () => {
         if (props.competition.persons) {
-            const generatedRanking = generateRanking(props.competition.persons, props.event, props.type);
+            const generatedRanking = generateRanking(props.competition.persons, props.event, type);
             console.log(generatedRanking);
             setRanking(generatedRanking);
         }
     };
-
+    const handleTypeChange = () => {
+        if (type === 'average') {
+            setType('single');
+        } else {
+            setType('average');
+        }
+    }
     useEffect(() => {
         generateRankingData();
-    }, [props.competition.persons]);
+    }, [props.competition.persons, type]);
     return (
         <>
             <Box sx={{mb: 5, mt: 2}}>
                 <Typography variant="h5">Psychsheet</Typography>
+                <Button variant="contained" onClick={handleTypeChange}>Single/average</Button>
+                <Typography variant="h6">Sorted by {type}</Typography>
                 <div className={classes.rankingTable}>
                     <Table>
                         <TableHead>
